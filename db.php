8902984db793e58723772d97ed824f8f1ae20758
LucaRiccio <lucariccio888@gmail.com>
2020-09-18 16:15:13 +0200

    database e stampa con foreach (senza include)

---
+++ db.php
@@ -0,0 +1,67 @@
+<?php
+
+  $database = [
+    [
+      'title' => 'New Jersey',
+      'author' => 'Bon Jovi',
+      'year' => 1988,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+    ],
+
+    [
+      'title' => 'Live at Wembley 86',
+      'author' => 'Queen',
+      'year' => 1992,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/71g40mlbinL._SX355_.jpg'
+    ],
+
+    [
+      'title' => 'Ten\'s Summoner\'s Tales',
+      'author' => 'Sting',
+      'year' => 1993,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/411BQR6BHRL.jpg'
+    ],
+
+    [
+      'title' => 'Steve Gadd band',
+      'author' => 'Steve Gadd Band',
+      'year' => 2018,
+      'poster' => 'https://m.media-amazon.com/images/I/81UtLzBDoEL._SS500_.jpg'
+    ],
+
+    [
+      'title' => 'Brave new World',
+      'author' => 'Iron Maiden',
+      'year' => 2000,
+      'poster' => 'https://cdn.shopify.com/s/files/1/0024/9803/5810/products/390933-Product-0-I_3bb66c1a-c297-4755-a66b-df0d80219d1e_large.jpg'
+    ],
+
+    [
+      'title' => 'One more car, one more rider',
+      'author' => 'Eric Clapton',
+      'year' => 2002,
+      'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
+    ]
+  ];
+
+?>
+
+ <!DOCTYPE html>
+ <html lang="en" dir="ltr">
+   <head>
+     <meta charset="utf-8">
+     <title></title>
+   </head>
+   <body>
+
+     <div class="cd">
+        <?php foreach ($database as $key => $cd){ ?>
+          <img src="<?php echo $cd['poster']; ?>" alt="">
+          <h3><?php echo $cd['title']; ?></h3>
+          <p><?php echo $cd['author']; ?></p>
+          <p><?php echo $cd['year']; ?></p>
+        <?php } ?>
+     </div>
+
+   </body>
+ </html>
