921a4f276633eb81ad78ad4bf042f74bacc0acf1
LucaRiccio <lucariccio888@gmail.com>
2020-09-18 16:27:25 +0200

    Milestone 1

--- db.php
+++ db.php
@@ -43,25 +43,3 @@
       'poster' => 'https://images-na.ssl-images-amazon.com/images/I/81MDAIdh78L._SY355_.jpg'
     ]
   ];
-
-?>
-
- <!DOCTYPE html>
- <html lang="en" dir="ltr">
-   <head>
-     <meta charset="utf-8">
-     <title></title>
-   </head>
-   <body>
-
-     <div class="cd">
-        <?php foreach ($database as $key => $cd){ ?>
-          <img src="<?php echo $cd['poster']; ?>" alt="">
-          <h3><?php echo $cd['title']; ?></h3>
-          <p><?php echo $cd['author']; ?></p>
-          <p><?php echo $cd['year']; ?></p>
-        <?php } ?>
-     </div>
-
-   </body>
- </html>
--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php
+  include 'db.php';
+?>
+
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -5,6 +9,15 @@
     <title>php-ajax-dischi</title>
   </head>
   <body>
+
+    <div class="cd">
+       <?php foreach ($database as $key => $cd){ ?>
+         <img src="<?php echo $cd['poster']; ?>" alt="">
+         <h3><?php echo $cd['title']; ?></h3>
+         <p><?php echo $cd['author']; ?></p>
+         <p><?php echo $cd['year']; ?></p>
+       <?php } ?>
+    </div>
 
   </body>
 </html>
